73a91d0c51b4309f22e908b2a8010aa01dfd94b9
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-14 23:17:21 +0530

    update the sum of of first n natural number

--- sumof.java
+++ sumof.java
@@ -1,3 +1,4 @@
+// This program calculates the sum of the first n natural numbers, where n is provided by the user as input.
 import java.util.*;
 public class sumof {
   public static void main(String[] args)
