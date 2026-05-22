153c95d3f823487a8b7f9cfc43d431293e8091e4
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-22 23:12:09 +0530

    update A static member is a member of a class that belongs to the class itself rather than to any specific instance of the class.

--- static_member.java
+++ static_member.java
@@ -1,3 +1,4 @@
+// A static member is a member of a class that belongs to the class itself rather than to any specific instance of the class.
 class demo {
     static int count = 0;
     static void increment()
