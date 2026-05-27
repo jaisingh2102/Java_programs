88af5573e58abf3c181db2bd76bcaee0ab5e7fe1
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-27 23:26:04 +0530

    update This program demonstrates the use of a do-while loop in Java.

--- dowhileloop.java
+++ dowhileloop.java
@@ -1,3 +1,4 @@
+// This program demonstrates the use of a do-while loop in Java. It initializes a counter variable and uses the loop to print a message a specified number of times, incrementing the counter with each iteration until a certain condition is met.
 public class dowhileloop {
     public static void main(String[] args)
     {
