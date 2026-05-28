b7bef1b317c184fd8b6e37f93f86973fc22f8399
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-28 22:44:56 +0530

    update Java program to demonstrate the use of a for loop.

--- forloop.java
+++ forloop.java
@@ -1,3 +1,4 @@
+// Java program to demonstrate the use of a for loop. The program prompts the user to enter a number and then uses a for loop to print the values of 'i' from 0 to that number, inclusive. Each value of 'i' is printed on a new line, showing the progression of the loop as it iterates through the specified range.
 import java.util.Scanner;
 public class forloop {
     public static void main(String[] args)
