2994126afd2ea032f630e1024d712450ea63d374
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-30 22:38:22 +0530

    update This code defines a class named 'student' with an integer field 'id' and a constructor to initialize it.

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-// Java program to illustrate the concept of constructor
+// This code defines a class named 'student' with an integer field 'id' and a constructor to initialize it. The class also has a method 'display' that prints the value of 'id'. In the 'Main' class, an instance of 'student' is created with an id of 101, and the 'display' method is called to show the id on the console.
 class student{
     int id;
     student(int id)
