2b70900d42cb8c99c880ad7be6ccc276776417f0
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-30 22:44:00 +0530

    update This code defines a class named 'printgreeting' that prompts the user to enter an integer.

--- printgreeting.java
+++ printgreeting.java
@@ -1,4 +1,4 @@
-// This program takes an integer input from the user and prints a greeting based on the value of the input.
+// This code defines a class named 'printgreeting' that prompts the user to enter an integer. Based on the input, it prints a greeting message in different languages. If the user enters 1, it prints "hello". If the user enters 2, it prints "namaste". For any other input, it prints "bonjour". The program uses a Scanner to read user input and closes the scanner at the end to prevent resource leaks.
 import java.util.Scanner;
 public class printgreeting {
    public static void main(String[]args)
